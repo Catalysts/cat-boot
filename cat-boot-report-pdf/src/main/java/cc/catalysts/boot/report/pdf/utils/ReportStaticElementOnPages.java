7ec1e208a52ba8e45a2ec6d5aa4a296cd210a3e4
sfarcas <Silviu.Farcas@catalysts.cc>
2016-08-01 19:27:07 +0300

    Javadoc

--- cat-boot-report-pdf/src/main/java/cc/catalysts/boot/report/pdf/utils/ReportStaticElementOnPages.java
+++ cat-boot-report-pdf/src/main/java/cc/catalysts/boot/report/pdf/utils/ReportStaticElementOnPages.java
@@ -2,6 +2,10 @@ package cc.catalysts.boot.report.pdf.utils;
 
 /**
  * Created by sfarcas on 7/21/2016.
+ *
+ * position of the footer or the header:
+ * on all pages, on all pages except the first, on all pages except the last
+ *
  */
 public enum ReportStaticElementOnPages {
     ALL, ALL_BUT_FIRST, ALL_BUT_LAST
